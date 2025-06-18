3397cba2962eab689c2928dbef3a752439bed890
Anjitha343 <anjithaayyappankutty@gmail.com>
2025-06-18 19:02:28 +0530

    Ensure auth routes included

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const allowedOrigins = [
-  "http://localhost:3000",
+  
   "https://project-manager-fronten.netlify.app/"
 ];
 const app = express();
